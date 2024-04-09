ef6c1d996885a3b3dc64d4d3cc359f90a70f2891
esmail <you@example.com>
2024-04-09 20:49:14 +0200

    remove handler

--- source_code/handler.c
+++ source_code/handler.c
@@ -2,8 +2,33 @@
 
 #include "headers/LinkedList.h"
 
-void AddHandler( List *list); 
-void RemoveHandler( List *list); 
+void AddHandler( List *list);
+void RemoveHandler(List *list) {
+    int x = 1;
+    while (x != 0) {
+        TraverseList(list,&displayKey);
+        printf("enter your choice : ");
+        scanf("%d", &x);
+
+        if (x == 0)
+            {
+            break;
+            }
+        else if (x < 1 || x > list->size)
+            {
+            return ;
+            }
+        else if (x==list->size+1)
+        {
+            DestroyList(list);
+        }
+         else
+         {
+            removeKey(x - 1, list);
+         }
+    }
+}
+
 void ModificationHandler( List *list);
 void DisplayHandler( List *list);
 
