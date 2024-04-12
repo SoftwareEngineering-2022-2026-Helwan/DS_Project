8a5b562ac27989c5843f43395a52a38280599fb4
Ahmed20225120 <medo632003@gmail.com>
2024-04-12 23:56:07 +0200

    Fixing Included libs

--- source_code/operations.c
+++ source_code/operations.c
@@ -1,7 +1,10 @@
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "../headers/List.h"
 //#include "../headers/sort.h"
 #include <string.h>
+#include "../headers/displayMenu.h"
+
 
 void prepareValueList(ListEntry *newPair)
 {
