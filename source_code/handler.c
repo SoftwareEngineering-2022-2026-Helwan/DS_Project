e70259da925572062260e2dbfcc02a84c749dab9
demiana674 <151900254+demiana674@users.noreply.github.com>
2024-04-13 00:05:52 +0200

    Update handler.c

--- source_code/handler.c
+++ source_code/handler.c
@@ -31,7 +31,33 @@ void RemoveHandler(List *list) {
 }
 
 void ModificationHandler( List *list);
-void DisplayHandler( List *list);
+void DisplayHandler( List *list){
+    ListEntry data;
+     int option;
+    do{  
+        TraverseList(list,&displayKey);
+        printf("enter your choice : ");
+        scanf("%d", &option);
+       switch(optio){
+         case 1:
+            int pos;
+             printf("\nEnter a Id to Insert : ");
+             scanf(" %d",&pos);
+             RetrieveList(&list,pos,&data);
+            break;
+         case 2:
+            displayKeyHeader(data);
+            break;
+         case 3:
+           TraverseList(list,&displayValue);
+            break;
+         case 4:
+            return 1;
+            break;
+       }
+    }while(option != 4);
+    
+};
 
 
 
